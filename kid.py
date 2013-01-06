4a6600f9fdcffef8413b768a9636af949b282db0
LAB STUDENT <lab@to-be-renamed.(none)>
2013-01-06 17:42:50 +0400

    merge groupify

--- kid.py
+++ kid.py
@@ -10,7 +10,7 @@ even_list = []
 for i in range(len(sim.light_list)):
 	if sim.light_list[i].id_code%2 == 0:
 		even_list.append(sim.light_list[i])
-even_group = sim.Group(even_list,True)
+even_group = sim.Group(True, even_list)
 
 ########## Odd Group ############
 
@@ -18,11 +18,11 @@ odd_list = []
 for i in range(len(sim.light_list)):
 	if sim.light_list[i].id_code%2 != 0:
 		odd_list.append(sim.light_list[i])
-odd_group = sim.Group(odd_list,True)
+odd_group = sim.Group(True, odd_list)
 
 ########## All Group ############
 
-all_group = sim.Group(sim.light_list,False)
+all_group = sim.Group(False, sim.light_list)
 
 #################################
 
@@ -32,17 +32,20 @@ ring_list = []
 ring_numbs = [(0,4),(5,20),(21,36),(37,52),(53,68),(69,112),(113,166),(167,230),(231,296),(297,324),(325,332)]
 
 for r in range(len(ring_numbs)):
-    ring_list.append(sim.Group([],False))
+    ring_list.append(sim.Group(False, []))
     for i in range(ring_numbs[r][0],ring_numbs[r][1] +1):
         ring_list[r].append(sim.light_list[i])
 
-ring_group = sim.Group(ring_list,False)
+ring_group = sim.Group(False, ring_list)
 
 #################################
 
+new_group = sim.Group.merge(True, odd_group, even_group)
 
-for i in range(ring_group.length()):
-    ring_group[i].set_color((255,0,0))
-    time.sleep(0.05)
+for i in range(new_group.length()):
+	new_group[i].set_color((255,0,0))
+	sim.flush_pygame()
+	time.sleep(0.1)
+
 
 sim.keep_run()
